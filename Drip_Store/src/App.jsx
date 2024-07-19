97091e08eb73e7378744ef2f7a9fe9004322514d
Vanessa <vanessaaraujo77077@gmail.com>
2024-07-19 01:07:24 -0300

    Falta ajustar o texto e chamar as imagens por fora

--- Drip_Store/src/App.jsx
+++ Drip_Store/src/App.jsx
@@ -1,14 +1,16 @@
-import { HomePage } from "./Pages/HomePage/HomePage"
-import { ProductListingPage } from "./Pages/ProductListingPage/ProductListingPage"
-import { ProductViewPage } from "./Pages/ProductViewPage/ProductViewPage"
+// import { HomePage } from "./Pages/HomePage/HomePage"
+// import { ProductListingPage } from "./Pages/ProductListingPage/ProductListingPage"
+// import { ProductViewPage } from "./Pages/ProductViewPage/ProductViewPage"
+import { Carousel } from "./Components/Carousel/Carousel"
 
 function App() {
 
   return (
     <>
-      <HomePage/>
+      {/* <HomePage/>
       <ProductListingPage/>
-      <ProductViewPage/>
+      <ProductViewPage/> */}
+      <Carousel/>
     </>
   )
 }
---
+++ Drip_Store/src/Components/Carousel/Carousel.jsx
@@ -0,0 +1,54 @@
+import "./Carousel.css"
+// Toda a instalação do Swiper  
+import { register } from 'swiper/element/bundle'
+
+register ()
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+export function Carousel() {
+    return(
+        <>
+            <Swiper 
+                slidesPerView={1}
+                pagination={{ clickable: true }}
+                navigation
+            >
+                <SwiperSlide className="SwiperSlide">
+                    <div className="slideGroup">
+                    <div className="infoSlide">
+                        <h5>Melhores Ofertas Personalizadas</h5>
+                        <h1>Queima de Estoque Nike 🔥</h1>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem, similique.</p>
+                        <button>Ver Ofertas</button>  
+                    </div>
+                    <div>
+                        <img src="public/TenisSlide.svg" className="slide-image" />
+                    </div>
+                    </div>
+                    
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div>
+                        texto2
+                    </div>
+                    <div>
+                        <img src="public/TenisSlide.svg" className="slide-image" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div>
+                        texto3
+                    </div>
+                    <div>
+                        <img src="public/TenisSlide.svg" className="slide-image" />
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+        </>
+    )
+}
