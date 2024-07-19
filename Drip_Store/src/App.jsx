e41dc2f08add174b58216b6e50c26bc2e406bbc3
Alisson Souza <alissonso432@gmail.com>
2024-07-19 14:37:04 -0300

    Footer feito

--- Drip_Store/src/App.jsx
+++ Drip_Store/src/App.jsx
@@ -1,12 +1,14 @@
 import { HomePage } from "./Pages/HomePage/HomePage"
 import { ProductListingPage } from "./Pages/ProductListingPage/ProductListingPage"
 import { ProductViewPage } from "./Pages/ProductViewPage/ProductViewPage"
+import { Footer } from "./Components/Footer/Footer"
 
 function App() {
 
   return (
     <>
       <HomePage/>
+      <Footer/>
       <ProductListingPage/>
       <ProductViewPage/>
     </>
