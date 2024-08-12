f3573336b43c0f497df31b35f7ef167e2acdebd3
unknown <VANESSA0823@digitalcollegesul.local>
2024-08-12 16:55:06 -0300

    iniciando criação do CreateAccount

---
+++ Drip_Store/src/Components/CadUser/CadUser.jsx
@@ -0,0 +1,13 @@
+import './CadUser.jsx'
+
+export function CadUser() {
+    return (
+        <div className='container-CadUser'>
+            <HeaderInicio/>
+            <h2>Criar Conta</h2>
+            <h6>Informações Pessoais</h6>
+            <input type="text" />
+            <label>nome</label>
+        </div>
+    )
+}
--- Drip_Store/src/Components/Header/Header.jsx
+++ Drip_Store/src/Components/Header/Header.jsx
@@ -1,10 +1,22 @@
+import { useLocation } from 'react-router-dom';
 import cart from '../../assets/mini-cart.svg';
 import IconSearch from '../../assets/Search.svg'
 import { Logo } from '../Logo/Logo';
 import './Header.css'
 import { NavBar } from '../Navbar/NavBar';
+import { Link } from 'react-router-dom';
 
 export function Header() {
+
+    const location = useLocation();
+
+    const isActive = (url) => {
+        if (location.pathname === url) {
+            return "active";
+        }
+        return "";
+    }
+
     return (
         <>
             <header>
@@ -13,8 +25,9 @@ export function Header() {
                     <input type="text" name="" id="" placeholder="Pesquisar produto..." />
                     <img className='search' src={IconSearch} alt="" />
                     <div>
-                        <a href="">Cadastre-se</a>
-                        <button>Entrar</button>
+                    {/* <Link to='/products' className={isActive('/products')}>Produtos</Link> */}
+                        <Link to="/CreateAccount" >Cadastre-se</Link>
+                        <button><Link to="/login">Entrar</Link></button>
                     </div>
                     <img className='cart' src={cart} alt="Icone de Carrinho de Compra" />
                 </div>
